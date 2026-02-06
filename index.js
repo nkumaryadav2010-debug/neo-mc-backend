f03abe7468ecb7e40788cd2dafbede01524ea497
nkumaryadav2010-debug <nkumaryadav2010@gmail.com>
2026-02-06 20:53:19 +0530

    Refactor index.js by removing CORS and changing response
    Removed CORS middleware and updated response message.

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
 import express from "express";
-import cors from "cors";
 
 const app = express();
-app.use(cors());
-app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Backend is working ✅");
+  res.send("Backend is working");
 });
 
-app.get("/status", (req, res) => {
-  res.json({
-    server: "NEO MC",
-    online: true,
-    players: 15,
-    maxPlayers: 100,
-    tps: 19.9
-  });
-});
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log("Server running on", PORT);
 });
